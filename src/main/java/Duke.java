e6592215f924b9365a44cc334fe0dd7bde46f313
DesmondKJL <kwa_jun_liang_desmond@s2013.sst.edu.sg>
2020-08-27 23:52:50 +0800

    insert greet echo exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,7 +10,24 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\n");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+
+
+        String str1 = "bye";
+        String line;
+        line = in.nextLine();
+        int var1 = str1.compareTo( line );
+
+        if (var1 == 0){
+            System.out.println("Bye. Hope to see you again soon!\n");
+        }
+
+        else{
+            System.out.println(line);
+        }
+
+
+
+
 
     }
 }
